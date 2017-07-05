a38c0d67279604dc28c6ff6b30f4b225491ae5b1
fedefabbri <fede.fabbri289@gmail.com>
2017-07-05 15:08:51 +0200

    Update bluefin rasterToPoly.R

--- R/bluefin rasterToPoly.R
+++ R/bluefin rasterToPoly.R
@@ -15,26 +15,53 @@ plot(bluefin)
 
 hist(bluefin)
 
-
 # 1) create extent with a bounding box
+
+westmed <- readOGR(dsn="C:\\Users\\fede\\Desktop\\MSP Master Tesis\\data",layer="WestMed_area")
+
 # 2) use crop() to subset raster for our study area.
 
+# wm_bluefin <- crop(bluefin, westmed)
 
+## for some reason this clip only for the whole Mediterranean sea not just the West Med
 ###reclassify
 
-v.rcl <- c(0, 0.1, 0, 0.1, 100, 1)
+v.rcl <- c(0, 10, 0, 10, 100, 1)
 m.rcl <- matrix(v.rcl, ncol=3, byrow=TRUE)
 
 bluefin.rec <- reclassify(bluefin, m.rcl)
 bluefin.rec[bluefin.rec==0]<-NA
-plot(bluefin.rec)
+plot(bluefin.rec, axes=T, col='red'); plot(westmed, add=T)
 
-## TRY TO CHANGE THE DISSOLVE OPTION FROM FALSE TO TRUE (still takes 1 hour time)
+## convert raster into polygon
 poly.bluefin <- rasterToPolygons(bluefin.rec, na.rm=TRUE, digits=12, dissolve=TRUE)
 
+# split multipolygon in separate polygons
+
+split.poly <- disaggregate(poly.bluefin)
+
 # 3) calculate area for each polygon
+
+bluefin.laea <- spTransform(split.poly,CRS("+init=EPSG:3035"))
+
+bluefin.laea$area <- area(bluefin.laea)
+
 # 4) select polygon with area higher than our specified threshold
 
+## threshold 100 km
 
-writePolyShape(poly.bluefin,"C:\\Users\\fede\\Desktop\\MSP Master Tesis\\data2")
+bluefin_over100 <- subset(bluefin.laea, area > 100000000)
 
+# dissolve the polygons in one multypoligon feature
+
+bluefin.uni <- gUnaryUnion(bluefin_over100)
+
+## make the clip again for WestMed, this way works fine
+
+westmed.laea <- spTransform(westmed,CRS("+init=EPSG:3035"))
+
+clip <- gIntersection(bluefin.uni, westmed.laea)
+
+plot(clip, axes=T); plot(westmed.laea, add=T);
+
+# writePolyShape(poly.bluefin,"C:\\Users\\fede\\Desktop\\MSP Master Tesis\\data2")
